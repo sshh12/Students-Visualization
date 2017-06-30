82358fdf69808993ab61f88cd2f8385410f0d0f9
Shrivu Shankar <shrivu1122@gmail.com>
2017-06-30 14:27:57 -0500

    Formatting + Minor Modifications

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-var colors = {
+var colors = { // R,G,B
     'red': [211, 47, 47],
     'blue': [57, 73, 171],
     'green': [56, 142, 60],
@@ -32,6 +32,7 @@ var coloring = 'random';
 var filtering = 'none';
 
 function setup() { // init()
+
     canvas = createCanvas(windowWidth, windowHeight, WEBGL);
     canvas.parent("app");
 
@@ -43,6 +44,7 @@ function setup() { // init()
 
     colorkey = document.getElementById("colorkey");
     filtersel = document.getElementById("filtersel");
+
 }
 
 function centerPoints() { // Verify and Correct Points to be around (0,0,0)
@@ -65,7 +67,7 @@ function centerPoints() { // Verify and Correct Points to be around (0,0,0)
         points[i][0] -= centX;
         points[i][1] -= centY;
         points[i][2] -= centZ;
-		points[i][0] *= 100;
+        points[i][0] *= 100;
         points[i][1] *= 100;
         points[i][2] *= 100;
     }
@@ -87,6 +89,7 @@ function draw() { // Update Screen
     for (var i = 0; i < points.length; i++) {
 
         if (notFiltered(i)) {
+
             push();
 
             translate(points[i][0], points[i][1], points[i][2]);
@@ -101,6 +104,7 @@ function draw() { // Update Screen
             }
 
             pop();
+
         }
 
     }
@@ -110,6 +114,7 @@ function draw() { // Update Screen
 /// Look Up Methods and Option Setters
 
 function setFilter(f) {
+
     filtering = f;
 
     if (filtering === "none") {
@@ -121,23 +126,31 @@ function setFilter(f) {
         filtersel.innerHTML = "Only Users with All Data";
 
     }
+
 }
 
 function notFiltered(index) {
+
     if (filtering === "none") {
+
         return true;
+
     } else if (filtering === "fulldata") {
+
         return labels[index][1] != "?";
+
     }
+
     return false;
+
 }
 
-function wrapColor(txt, color) {
-    return "<span style='color:rgb(" + color[0] + "," + color[1] + "," + color[2] + ")'>" + txt + "</span>";
-}
+let wrapColor = (txt, color) => (`<span style='color:rgb(${color[0]},${color[1]},${color[2]})'>${txt}</span>`);
 
 function setColor(c) {
+
     coloring = c;
+
     if (coloring === "random") {
 
         colorkey.innerHTML = 'Random Colors';
@@ -147,64 +160,66 @@ function setColor(c) {
         var subcolors = Object.values(colors);
 
         colorkey.innerHTML = wrapColor("Science", subcolors[0]) + " " +
-            wrapColor("Math", subcolors[1]) + " " +
-            wrapColor("English", subcolors[2]) + " " +
-            wrapColor("Language", subcolors[3]) + " " +
-            wrapColor("Art", subcolors[4]) + " " +
-            wrapColor("Sports", subcolors[5]) + " " +
-            wrapColor("Social Studies", subcolors[6])  + " " +
-			wrapColor("Engineering", subcolors[7]);
+                             wrapColor("Math", subcolors[1]) + " " +
+                             wrapColor("English", subcolors[2]) + " " +
+                             wrapColor("Language", subcolors[3]) + " " +
+                             wrapColor("Art", subcolors[4]) + " " +
+                             wrapColor("Sports", subcolors[5]) + " " +
+                             wrapColor("Social Studies", subcolors[6]) + " " +
+                             wrapColor("Engineering", subcolors[7]);
 
     } else if (coloring === "lowest") {
 
         var subcolors = Object.values(colors);
 
         colorkey.innerHTML = wrapColor("Science", subcolors[0]) + " " +
-            wrapColor("Math", subcolors[1]) + " " +
-            wrapColor("English", subcolors[2]) + " " +
-            wrapColor("Language", subcolors[3]) + " " +
-            wrapColor("Art", subcolors[4]) + " " +
-            wrapColor("Sports", subcolors[5]) + " " +
-            wrapColor("Social Studies", subcolors[6])  + " " +
-			wrapColor("Engineering", subcolors[7]);
+                             wrapColor("Math", subcolors[1]) + " " +
+                             wrapColor("English", subcolors[2]) + " " +
+                             wrapColor("Language", subcolors[3]) + " " +
+                             wrapColor("Art", subcolors[4]) + " " +
+                             wrapColor("Sports", subcolors[5]) + " " +
+                             wrapColor("Social Studies", subcolors[6]) + " " +
+                             wrapColor("Engineering", subcolors[7]);
 
     } else if (coloring === "dist") {
 
         colorkey.innerHTML = wrapColor("Close", colors.blue) + " " +
-            wrapColor("to", colors.green) + " " +
-            wrapColor("Far", colors.red);
+                             wrapColor("to", colors.green) + " " +
+                             wrapColor("Far", colors.red);
 
     } else if (coloring === "gender") {
 
         colorkey.innerHTML = wrapColor("Male", colors.blue) + " " +
-            wrapColor("Female", colors.pink);
+                             wrapColor("Female", colors.pink);
 
     } else if (coloring === "grade") {
 
         colorkey.innerHTML = wrapColor("<9", colors.yellow) + " " +
-            wrapColor("9", colors.blue) + " " +
-            wrapColor("10", colors.green) + " " +
-            wrapColor("11", colors.purple) + " " +
-            wrapColor("12", colors.red);
+                             wrapColor("9", colors.blue) + " " +
+                             wrapColor("10", colors.green) + " " +
+                             wrapColor("11", colors.purple) + " " +
+                             wrapColor("12", colors.red);
 
     } else if (coloring === "language") {
 
         colorkey.innerHTML = wrapColor("English", colors.blue) + " " +
-            wrapColor("Spanish", colors.yellow) + " " +
-            wrapColor("Chinese", colors.red) + " " +
-            wrapColor("Arabic", colors.green) + " " +
-            wrapColor("Vietnamese", colors.orange) + " " +
-            wrapColor("Bengali", colors.purple) + " " +
-            wrapColor("Indonesian", colors.teal) + " " +
-            wrapColor("Russian", colors.pink);
+                             wrapColor("Spanish", colors.yellow) + " " +
+                             wrapColor("Chinese", colors.red) + " " +
+                             wrapColor("Arabic", colors.green) + " " +
+                             wrapColor("Vietnamese", colors.orange) + " " +
+                             wrapColor("Bengali", colors.purple) + " " +
+                             wrapColor("Indonesian", colors.teal) + " " +
+                             wrapColor("Russian", colors.pink);
 
     }
+
 }
 
 var dists = [];
 var maxDist = 0;
 
 function getColor(index) {
+
     if (coloring === "random") {
 
         return [index % 210 + 45, index * 7 % 210 + 45, index * index % 210 + 45];
@@ -224,15 +239,14 @@ function getColor(index) {
         if (dists.length === 0) {
 
             maxDist = 0;
-            for (var p = 0; p < points.length; p++) {
+            for (let point of points) {
 
                 var dist = 0;
-                for (var i = 0; i < selected.length; i++) {
+                for (let sIndex of selected) {
 
-                    var sIndex = selected[i];
-                    dist += pow(points[p][0] - points[sIndex][0], 2) +
-                        pow(points[p][1] - points[sIndex][1], 2) +
-                        pow(points[p][2] - points[sIndex][2], 2);
+                    dist += (pow(point[0]                  - points[sIndex][0], 2) +
+                             pow(point[1] - points[sIndex][1], 2) +
+                             pow(point[2] - points[sIndex][2], 2));
 
                 }
 
@@ -297,30 +311,42 @@ function getColor(index) {
     }
 
     return [255, 255, 255];
+
 }
 
 /// Events
 
 function mouseDragged() { // Orbit/Pan
+
     if (mouseButton === RIGHT) {
+
         camX += (pmouseX - mouseX) / 10;
         camY += (pmouseY - mouseY) / 10;
+
     } else if (mouseButton === LEFT) {
+
         rotY += (pmouseX - mouseX) / 200;
         rotX += (pmouseY - mouseY) / 200;
+
     }
+
 }
 
 function mouseWheel(ev) { // Zoom
+
     scl -= ev.delta / 1200;
     scl = constrain(scl, .1, 6);
+
 }
 
 function windowResized() {
+
     resizeCanvas(windowWidth, windowHeight);
+
 }
 
 function updateUserInput() { // Update Selection
+
     selected = [];
     var vals = userinput.value().replace("s", "").replace(" ", "").split(",").map(hash);
     if (vals.length >= 1) {
@@ -330,13 +356,13 @@ function updateUserInput() { // Update Selection
             }
         }
     }
-    document.getElementById("userlabel").innerHTML = "User (" + selected.length + ")";
+    document.getElementById("userlabel").innerHTML = `Users (${selected.length})`;
     dists = [];
+
 }
 
-
-function hash(val) {
-    var shaObj = new jsSHA("SHA-256", "TEXT");
-    shaObj.update(val);
+let hash = (value) => {
+    let shaObj = new jsSHA("SHA-256", "TEXT");
+    shaObj.update(value);
     return shaObj.getHash("HEX");
 }
