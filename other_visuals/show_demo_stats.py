973c18976437b4d82e250c8920c23a1790c19446
Shrivu Shankar <shrivu1122@gmail.com>
2018-01-17 16:31:50 -0600

    Updated Data

--- other_visuals/show_demo_stats.py
+++ other_visuals/show_demo_stats.py
@@ -18,9 +18,10 @@ def create_plot():
     """Plots with matplot lib"""
     import matplotlib.pyplot as plt
 
-    def make_bar(plot, data_dict, title, limit=None, color=None):
+    def make_bar(plot, data_dict, title, limit=None, color=None, keys=None):
 
-        keys = sorted(data_dict.keys(), key=lambda key: -data_dict[key])
+        if not keys:
+            keys = sorted(data_dict.keys(), key=lambda key: -data_dict[key])
 
         if limit:
             keys = keys[:limit]
@@ -28,9 +29,16 @@ def create_plot():
         indexes = list(range(len(keys)))
 
         plot.set_title(title)
-        plot.bar(indexes, [data_dict[k] for k in keys], color=color)
+        bars = plot.bar(indexes, [data_dict[k] for k in keys], color=color)
         plot.set_xticks(indexes)
         plot.set_xticklabels([k.title() for k in keys])
+
+        for rect in bars:
+            height = rect.get_height()
+            plot.text(rect.get_x() + rect.get_width() / 2.,
+                    height + 10,
+                    '%d' % int(height),
+                    ha='center', va='bottom')
 
 
     plt.style.use('ggplot')
@@ -38,8 +46,8 @@ def create_plot():
     f, ((ax1, ax2, ax3, ax4)) = plt.subplots(4, 1)
 
     make_bar(ax1, schools, 'School', 5)
-    make_bar(ax2, grades, 'Grade Level')
-    make_bar(ax3, genders, 'Gender')
+    make_bar(ax2, grades, 'Grade Level', keys=['12', '11', '10', '9'])
+    make_bar(ax3, genders, 'Gender', color=['#E53935', '#3949AB'])
     make_bar(ax4, languages, 'Language', 5)
 
     plt.show()
